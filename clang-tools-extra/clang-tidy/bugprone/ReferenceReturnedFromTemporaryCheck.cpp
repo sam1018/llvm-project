cd9fa65acc24d9adf021732356388d537e5ec443
Samiul Hoque <samiul1018@gmail.com>
2020-07-14 08:55:11 +0200

    Check first argument of opetaror call for temporaries.

--- clang-tools-extra/clang-tidy/bugprone/ReferenceReturnedFromTemporaryCheck.cpp
+++ clang-tools-extra/clang-tidy/bugprone/ReferenceReturnedFromTemporaryCheck.cpp
@@ -9,6 +9,7 @@
 #include "ReferenceReturnedFromTemporaryCheck.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
+#include <iostream>
 
 using namespace clang::ast_matchers;
 
@@ -22,22 +23,36 @@ AST_MATCHER(Expr, isLValue) { return Node.isLValue(); }
 
 namespace {
 std::vector<const Stmt *> getChildren(const Stmt *stmt) {
-  if (isa<CallExpr>(stmt) && !isa<CXXOperatorCallExpr>(stmt)) {
+  // we don't want to check function arguments for temporaries, as it is quite
+  // common to pass temporaries as function parameters... this means, we won't
+  // be able to check cases like below unfortunately...
+  //
+  // int& get(const X& x) { return x.val; }
+  // const int& val = get(X{});
+  if (isa<CallExpr>(stmt)) {
     const auto *callExpr = dyn_cast<CallExpr>(stmt);
     llvm::SetVector<const Stmt *> all_args(callExpr->arg_begin(),
                                            callExpr->arg_end());
     llvm::SetVector<const Stmt *> non_arg_children(callExpr->child_begin(),
                                                    callExpr->child_end());
 
+    // std::cout << callExpr->getStmtClassName() << "," << all_args.size() <<
+    // "\n";
+
     auto size1 = non_arg_children.size();
     non_arg_children.set_subtract(all_args);
     auto size2 = non_arg_children.size();
 
-    // temporary... should be assert, checking like below as I am only doing
-    // release builds now...
     if (size1 != size2 + all_args.size())
       llvm::report_fatal_error(
           "Unexpected. Set subtraction produced wrong result.");
+
+    if (isa<CXXOperatorCallExpr>(stmt)) {
+      // this is the one case where we definitely want to check the function
+      // argument for temporaries, so we don't miss out on operators like: ->,
+      // *, or []... and arg 0 is the caller of the operator...
+      non_arg_children.insert(callExpr->getArg(0));
+    }
 
     return non_arg_children.takeVector();
   }
@@ -84,7 +99,8 @@ BranchState GetBranchState(const Stmt *stmt, const ASTContext &Context) {
   if (isa<CXXMemberCallExpr>(stmt) || isa<CXXOperatorCallExpr>(stmt)) {
     const auto *MemberCall = dyn_cast<CallExpr>(stmt);
     const auto &RetType = MemberCall->getCallReturnType(Context);
-    if (RetType->isReferenceType() || RetType->isPointerType())
+    if (!RetType.isNull() &&
+        (RetType->isReferenceType() || RetType->isPointerType()))
       return BranchState::MayDangle;
     return BranchState::WontDangle;
   }
@@ -95,6 +111,9 @@ const MaterializeTemporaryExpr *GetTemporary(const Stmt *stmt,
                                              const std::string &TempWhiteListRE,
                                              const ASTContext &Context,
                                              BranchState bs) {
+
+  if (!stmt)
+    return nullptr;
 
   const auto *TempOb = dyn_cast<MaterializeTemporaryExpr>(stmt);
 
@@ -139,7 +158,8 @@ void ReferenceReturnedFromTemporaryCheck::storeOptions(
 void ReferenceReturnedFromTemporaryCheck::registerMatchers(
     MatchFinder *Finder) {
   Finder->addMatcher(
-      varDecl(hasType(lValueReferenceType()), unless(parmVarDecl()),
+      varDecl(unless(isExpansionInSystemHeader()),
+              hasType(lValueReferenceType()), unless(parmVarDecl()),
               hasInitializer(traverse(TK_AsIs, expr().bind("theInitializer"))))
           .bind("theVarDecl"),
       this);
@@ -148,7 +168,7 @@ void ReferenceReturnedFromTemporaryCheck::registerMatchers(
 void ReferenceReturnedFromTemporaryCheck::check(
     const MatchFinder::MatchResult &Result) {
   const auto *MatchedDecl = Result.Nodes.getNodeAs<VarDecl>("theVarDecl");
-  //MatchedDecl->dump();
+  // MatchedDecl->dump();
 
   const auto *TempOb =
       GetTemporary(Result.Nodes.getNodeAs<Expr>("theInitializer"),
@@ -160,14 +180,14 @@ void ReferenceReturnedFromTemporaryCheck::check(
   const auto *TempCXXDecl = TempOb->getType()->getAsCXXRecordDecl();
 
   if (!TempCXXDecl) {
-    diag(TempOb->getEndLoc(), "Matched: %0, Temporary is not CXXRecordDecl")
+    diag(TempOb->getEndLoc(), "Matched: %0, `Temporary is not CXXRecordDecl`")
         << MatchedDecl;
     return;
   }
 
   const auto &TempDeclName = TempCXXDecl->getQualifiedNameAsString();
 
-  diag(TempOb->getEndLoc(), "Matched: %0, Temporary Name: %1")
+  diag(TempOb->getEndLoc(), "Matched: %0, Temporary Name: `%1`")
       << MatchedDecl << TempDeclName;
 }
 
--- clang-tools-extra/test/clang-tidy/checkers/bugprone-reference-returned-from-temporary2.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bugprone-reference-returned-from-temporary2.cpp
@@ -2,10 +2,13 @@
 
 #include <optional>
 #include <string>
+#include <map>
 
 std::optional<std::pair<std::string, std::string>> f() { return std::make_optional(std::make_pair("abc", "def")); }
 
 const auto &match1 = f()->first;
-// CHECK-MESSAGES: :[[@LINE-1]]:24: warning: Matched: 'match1', Temporary Name: std::optional [bugprone-reference-returned-from-temporary]
+// CHECK-MESSAGES: :[[@LINE-1]]:24: warning: Matched: 'match1', Temporary Name: `std::optional` [bugprone-reference-returned-from-temporary]
 
 const auto &no_match1{std::to_string(10)};
+std::map<int, std::string> m;
+const auto &no_match2 = m[10];
