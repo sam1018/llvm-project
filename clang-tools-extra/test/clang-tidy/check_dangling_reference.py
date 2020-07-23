f610230a3dc9f8452a62171ac1cfa57e27b05e6f
Samiul Hoque <samiul1018@gmail.com>
2020-07-23 19:13:51 +0200

    ct test: run test from module.

--- clang-tools-extra/test/clang-tidy/check_dangling_reference.py
+++ clang-tools-extra/test/clang-tidy/check_dangling_reference.py
@@ -1,11 +1,11 @@
 import os
 import sys
 import subprocess
+import check_clang_tidy
 
 test_path = os.path.dirname(os.path.realpath(__file__))
 test_runner = f"{test_path}/check_clang_tidy.py"
 test_name = "bugprone-reference-returned-from-temporary"
-test_file_name_prefix = f"{test_path}/checkers/{test_name}"
 
 def extra_args(test_case):
     if test_case == 2:
@@ -28,16 +28,35 @@ def extra_args(test_case):
         return [f"-config={options}"]
     return []
 
-all_test_cases = range(1, 3)
-if len(sys.argv) > 1:
-    test_case_to_run = int(sys.argv[1])
-    if test_case_to_run in all_test_cases:
-        all_test_cases = [test_case_to_run]
+def expand_std(std):
+  if std == 'c++98-or-later':
+    return ['c++98', 'c++11', 'c++14', 'c++17', 'c++20']
+  if std == 'c++11-or-later':
+    return ['c++11', 'c++14', 'c++17', 'c++20']
+  if std == 'c++14-or-later':
+    return ['c++14', 'c++17', 'c++20']
+  if std == 'c++17-or-later':
+    return ['c++17', 'c++20']
+  if std == 'c++20-or-later':
+    return ['c++20']
+  return [std]
 
-for test_case in all_test_cases:
-    print(f'Running test case {test_case}')
-    cmd = ["python", test_runner, f"{test_file_name_prefix}{test_case}.cpp", test_name, "temp~"]
-    cmd.extend(extra_args(test_case))
-    print(f'Running test case {test_case}: {cmd}')
-    ret = subprocess.run(cmd)
-    ret.check_returncode()
+class CTCheckArgs:
+    def __init__(self, test_case):
+        self.assume_filename = None
+        self.check_name = 'bugprone-reference-returned-from-temporary'
+        self.check_suffix = ['']
+        self.expect_clang_tidy_error = False
+        test_file_name_prefix = f'{test_path}/checkers/{test_name}'
+        self.input_file_name = f'{test_file_name_prefix}{test_case}.cpp'
+        self.resource_dir = None
+        self.std = ['c++11-or-later']
+        self.temp_file_name = 'temp~'
+
+for test_case in [1, 2]:
+    args = CTCheckArgs(test_case)
+    abbreviated_stds = args.std
+    for abbreviated_std in abbreviated_stds:
+        for std in expand_std(abbreviated_std):
+            args.std = std
+            check_clang_tidy.run_test_once(args, extra_args(test_case))
